1ff4e390607eb16bed917356bcabb40814e8faf7
Stefan Wallentowitz <stefan@wallentowitz.de>
2017-03-03 14:38:33 +0100

    Initial commit

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -e
+
+cd /tmpfiles/Xilinx_Vivado_SDK_${VERSION}_*
+./xsetup -l ${VIVADO_BASE} -b Install -c /tmp/webpack_config.txt -a XilinxEULA,3rdPartyEULA,WebTalkTerms
+
+cat <<EOF > ${VIVADO_MODULEFILES}/${VERSION}
+#%Module
+prepend-path PATH ${VIVADO_BASE}/Vivado/${VERSION}/bin
+EOF
