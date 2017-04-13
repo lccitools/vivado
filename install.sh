405e910fab71805381748e11ee6d4f42feccd6cd
Stefan Wallentowitz <stefan.wallentowitz@tum.de>
2017-04-13 15:14:39 +0200

    Create modulefiles directory if it doesn't exist

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 cd /tmpfiles/Xilinx_Vivado_SDK_${VERSION}_*
 ./xsetup -l ${VIVADO_BASE} -b Install -c /tmp/webpack_config.txt -a XilinxEULA,3rdPartyEULA,WebTalkTerms
 
+mkdir -p ${VIVADO_MODULEFILES}
 cat <<EOF > ${VIVADO_MODULEFILES}/${VERSION}
 #%Module
 prepend-path PATH ${VIVADO_BASE}/Vivado/${VERSION}/bin
